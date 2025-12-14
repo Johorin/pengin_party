e425fdacb0f47b06b38556fce9dcb1c42aff19ef
Yuya Hojo <hoyuak2145@docomo.ne.jp>
2025-12-15 02:14:54 +0900

    env調整

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-    "context"
-	"github.com/gin-gonic/gin"
+	"context"
+	"fmt"
+	"os"
+	"pengin_party/config"
+	"pengin_party/internal/application/usecases/user/usecase"
 	"pengin_party/internal/infrastructure/redis"
+	"pengin_party/internal/presentation/controllers"
+
+	"github.com/gin-gonic/gin"
+	// "pengin_party/di"
 )
 
 func main() {
+	config.LoadConfig()
+	// fmt.Println(config.DB.DNS())
+	fmt.Println("APP_ENVは：" + os.Getenv("APP_ENV"))
+
+	// Redisクライアントの初期化
 	ctx := context.Background()
 	rdb, err :=  redis.NewRedisClient(ctx)
 	if err != nil {
@@ -14,8 +26,12 @@ func main() {
 	}
 	defer rdb.Close()
 
+	user_uc   := usecase.NewCreateUserUseCase()
+	user_con  := controllers.NewUserController(user_uc)
+	// user_repo := 
+
 	router := gin.Default()
 	// router.POST("/login", )
-	router.POST("/users", )			// ユーザーの登録（to MySQL）
-	router.PUT("/rooms/{roomId}", )	// マッチング部屋を作成（to Redis）
+	router.POST("/users", user_con.Create)			// ユーザーの登録（to MySQL）
+	// router.PUT("/rooms/{roomId}", )	// マッチング部屋を作成（to Redis）
 }
