1da1064be22cc77f580b6e03f8b38ca3d3b877b1
Yuya Hojo <hoyuak2145@docomo.ne.jp>
2026-01-19 00:31:18 +0900

    createRoomAPIをフェッチ

--- frontend/my-app/app/top/create_room/page.tsx
+++ frontend/my-app/app/top/create_room/page.tsx
@@ -1,30 +1,55 @@
 'use client'
 
 import { useState, useEffect } from 'react';
+import { useCurrentUser } from '@/app/hooks/useCurrentUser';
 
 const CreateRoom = () => {
-    const [roomUid, setRoomUid] = useState<string>('');
+    const [roomId, setRoomId] = useState<string>('');
+    const { user, loading } = useCurrentUser();
 
     useEffect(() => {
-        // 6桁の英数字UIDを生成
-        const generateRoomUid = (): string => {
-            const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            let uid = '';
-            for (let i = 0; i < 6; i++) {
-                uid += chars.charAt(Math.floor(Math.random() * chars.length));
-            }
-            return uid;
-        };
+        if (loading == true || !user) return;
 
-        setRoomUid(generateRoomUid());
-    }, []);
+        async function createRoom(roomId: string) {
+            const idToken = await user?.getIdToken();
+            console.log("idToken: ", idToken);
+
+            const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT + '/rooms', {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${idToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "room_id": roomId
+                }),
+            });
+
+            const data = await res.json()
+            console.log("fetch POST /rooms: ", data);
+        }
+
+        // 6桁の英数字IDを生成
+        const generateRoomId = (): string => {
+            const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            let result = '';
+            for (let i = 0; i < 6; i++) {
+                result += chars.charAt(Math.floor(Math.random() * chars.length));
+            }
+            return result;
+        };
+        const roomId = generateRoomId();
+
+        createRoom(roomId);
+        setRoomId(roomId);
+    }, [user, loading]);
 
     return (
         <div className="random-room-uid">
-            {roomUid && (
+            {roomId && (
                 <div className="text-center">
                     <p className="text-2xl font-bold text-gray-900 dark:text-white">
-                        ルームID: <span className="text-blue-600 dark:text-blue-400">{roomUid}</span>
+                        ルームID: <span className="text-blue-600 dark:text-blue-400">{roomId}</span>
                     </p>
                 </div>
             )}
